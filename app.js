cd09c1570ff4683fa6bc1bd819699006dab9d726
jaisat <jaiswalsatyam211@gmail.com>
2021-05-10 15:13:06 +0530

    Nested Routes for comments added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require('express'),
     bodyParser  = require("body-parser"),
     mongoose    = require('mongoose'),
     Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
     seedDB      = require("./seed");
 
 seedDB();
@@ -20,7 +21,7 @@ app.get("/campgrounds",function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.render("index",{campgrounds:allcampgrounds});
+            res.render("campgrounds/index",{campgrounds:allcampgrounds});
         }
     });
  });
@@ -41,7 +42,7 @@ app.post("/campgrounds",function(req,res){
 });
 
 app.get("/campgrounds/new",function(req,res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW--- shows more info about one campground
@@ -50,11 +51,42 @@ app.get("/campgrounds/:id",function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.render("show",{campground:foundCampground});
+            res.render("campgrounds/show",{campground:foundCampground});
         }
     });
 });
 
+// ====================================
+// COMMENT ROUTES
+app.get("/campgrounds/:id/comments/new",function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new",{campground:campground});
+        }        
+    });
+});
+
+app.post("/campgrounds/:id/comments",function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            });
+        }
+    });
+});
+// ====================================
 app.listen(3000,function(){
     console.log("SERVER HAS STARTED!!");
 });
