6c5a9f7bbb39898c4bea35a27beecad0e780975c
jaisat <jaiswalsatyam211@gmail.com>
2021-05-11 17:34:25 +0530

    Delete added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -70,6 +70,18 @@ router.put("/:id",function(req,res){
     });
 });
 
+//DESTROY CAMPGROUND ROUTES
+
+router.delete("/:id",function(req,res){
+    Campground.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
