744b1c252689c2330ce12e253f5f5739c627fcb5
jaisat <jaiswalsatyam211@gmail.com>
2021-05-11 16:21:48 +0530

    cleaning up

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var commentRoutes    = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes       = require('./routes/index');
 
-seedDB();
+// seedDB();
 mongoose.connect("mongodb://localhost/campzone",{useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended:true}));
